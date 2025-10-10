339fca1249519cdd486cb31f066a5eecc5ecb2b9
Artiom Domnitsak <artiom.domnitsak.t0129854@edu.atec.pt>
2025-10-10 08:09:26 +0100

    horas e minutos

--- Exercicios/exercicio1.py
+++ Exercicios/exercicio1.py
@@ -1,10 +1,10 @@
-segundos = 0
-horas = 0
-minutos = 0
-
 segundos = int(input("Escreva os segundos: "))
 
-segundos * 60 = horas
-segundos 
+horas = segundos // 3600
+minutos = (segundos % 3600) // 60
+
+print("Horas",horas)
+print("Minutos",minutos)
+
 
 
