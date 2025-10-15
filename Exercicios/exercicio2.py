ca4f56b615ff71418f4eb374de3e314ece621827
Artiom Domnitsak <artiom.domnitsak.t0129854@edu.atec.pt>
2025-10-15 14:37:16 +0100

    ex2 e explicações

---
+++ Exercicios/exercicio2.py
@@ -0,0 +1,48 @@
+num1 = 0
+num2 = 0
+num3 = 0
+
+
+""""
+num1 = int(input("Escreva o primeiro número: "))
+num2 = int(input("Escreva o segundo número: "))
+num3 = int(input("Escreva o terceiro número: "))
+"""
+
+
+""""
+print(f"num1 valor = {num1}")
+print(type(num1))
+"""
+"""
+#Diferença entre dois numeros
+if num1>num2:
+    print("Numero 1 é maior")
+elif num1==num2:
+    print("Numero 1 é igual a numero 2")
+else:
+    print("Numero 2 é maior ")
+"""
+
+num1 = 0
+num2 = 0
+num3 = 0
+
+num1 = input("introduza um valor: ")
+num2 = input("introduza um valor: ")
+num3 = input("introduza um valor: ")
+
+
+if num1>num2>num3:
+    print("Numero 1 é maior")
+elif num2>num3:
+    print("Numero 2 é maior")
+else:
+    print("Numero 3 é maior ")
+
+if num1<num2<num3:
+    print("Numero 1 é menor")
+elif num2<num3:
+    print("Numero 2 é menor")
+else:
+    print("Numero 3 é menor ")
